a65071c65c5a709b330e003790dd699bc6307912
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-06 09:49:48 +0000

    Created supbase client

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,11 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
-export function createClient(){
-   return (
-      
-   )
+const projectUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const projectAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if(!projectUrl || !projectAnonKey){
+   throw new Error("Supabase environmental variables are missing!")
 }
+export const supabase = createClient(projectUrl, projectAnonKey)
+
+
