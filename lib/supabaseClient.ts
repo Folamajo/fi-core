8eb4f32fd8a83344f71b8b0ad9ae126c30ccd368
fmajoyegbe@gmail.com <fmajoyegbe@gmail.com>
2025-11-06 10:34:26 +0000

    changed supabase to typescript format

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,11 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/database.types";
 
 const projectUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const projectAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const projectAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if(!projectUrl || !projectAnonKey){
    throw new Error("Supabase environmental variables are missing!")
 }
-export const supabase = createClient(projectUrl, projectAnonKey)
 
+const supabase = createClient<Database>( projectUrl, projectAnonKey)
 
